f6f65568f68accd11952d4bf73d6fa5bd61527ac
DESKTOP-J7T20IR\Admin <shashwat.suyash@skillassure.com>
2022-10-12 18:50:32 +0530

    added comment

--- src/app/doctor-flow/profile/vet-pf/vet-pf.component.ts
+++ src/app/doctor-flow/profile/vet-pf/vet-pf.component.ts
@@ -1,3 +1,4 @@
+import { throwDialogContentAlreadyAttachedError } from '@angular/cdk/dialog';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -13,19 +14,26 @@ export class VetPfComponent implements OnInit {
 
   constructor(private vetService : VetServiceService, private router : Router) { }
   public curVet : Vet;
+  public url : string;
+  public data : any;
+  
   ngOnInit(): void {
-
+    //NgonIntit SUnil
     this.vetService.GetVet().subscribe((data)=> {
+      
+      this.data = data;
       console.log(data);
       console.log("Data fetched");
-      this.curVet = new Vet();
-      this.curVet.DoctorID = data.DoctorID;
-      this.curVet.Name = data.DoctorName;
-      this.curVet.Email = data.Email;
-      this.curVet.Clinic = data.Clinic;
-      this.curVet.Speciality = data.Speciality;
-      this.curVet.Mobile = data.Mobile;
-      this.curVet.NPI = data.NPI;
+      //this.url ="Vets/profile/EditVet/"+data.DoctorID;
+      // console.log(this.url);
+      // this.curVet = new Vet();
+      // this.curVet.DoctorID = data.DoctorID;
+      // this.curVet.Name = data.DoctorName;
+      // this.curVet.Email = data.Email;
+      // this.curVet.Clinic = data.Clinic;
+      // this.curVet.Speciality = data.Speciality;
+      // this.curVet.Mobile = data.Mobile;
+      // this.curVet.NPI = data.NPI;
     })
   }
 
@@ -33,7 +41,10 @@ export class VetPfComponent implements OnInit {
     //console.log(this.curVet.DoctorID);
     this.router.navigate(["vets/profile/EditVet/",this.curVet.DoctorID]);
     return;
+    console.log(this.data);//null
 
   }
+
+  
 
 }
